686740c228149899938c94bae3c28c7c711f0861
763613923@qq.com <763613923@qq.com>
2017-12-22 19:24:23 +0800

    fix promise List

--- jsnote/promise.js
+++ jsnote/promise.js
@@ -1,41 +1,80 @@
-/**
- * Promist 队列执行的一种简单实现
- */
-let initConnect = 10 //总的连接数
-let current = 3 //最大执行数
-let promiseList = []
-let successIndex = 0
-let testPromise = (index, listIndex) => {
-  return new Promise(resolve => {
-    let delay = Math.random() * 1000
-    setTimeout(() => {
-      resolve({
-        index,
-        listIndex
-      })
-    }, delay)
-  })
-}
-
-let createPromise = (i, v) => {
-  initConnect--
-  return testPromise(i, v).then(data => {
-    successIndex = data.listIndex
-  })
-}
-let initPromise = () => {
-  let index = 0
-  while (promiseList.length < current) {
-    promiseList.push(createPromise(initConnect, index++))
+class PromiseLists {
+  constructor(urls, current, allConnect) {
+    if (!urls || !this.isUrls(urls)) {
+      throw Error('urls is required')
+    } else {
+      this.urls = urls
+    }
+    // 默认发送urls与总的次数相等
+    this.allConnect = allConnect ? allConnect : this.urls.length
+    // simle is number
+    this.current = current && !isNaN(current) ? current : 3
+    this.promiseList = []
+    //init promiseList success index
+    this.successIndex = 0
+    //init callback
+    this.callBack = data => {
+      console.log('callBack:', data.url)
+    }
   }
-}
-let after = () => {
-  if (initConnect > 0) {
-    Promise.race(promiseList).then(() => {
-      promiseList[successIndex] = createPromise(initConnect, successIndex)
-      after()
+  isUrls(urls) {
+    // 判断是否是数组且是urls数组
+    return true
+  }
+  /**
+   * create promise
+   * @param {*} url promise urls
+   * @param {*} listIndex  promiseList Index
+   */
+  createPromise(url, listIndex) {
+    // 创建一个　减少一个
+    console.log('createPromise:', url)
+    this.allConnect--
+    return new Promise(resolve => {
+      let delay = Math.random() * 6000
+      setTimeout(() => {
+        resolve({
+          url,
+          listIndex
+        })
+      }, delay)
+    }).then(data => {
+      this.successIndex = data.listIndex
+      this.callBack(data)
     })
   }
+  initPromise() {
+    let index = 0
+    while (this.promiseList.length < this.current && this.allConnect > 0) {
+      this.promiseList.push(
+        //　倒着创建
+        this.createPromise(this.getPromiseUrl(), index++)
+      )
+    }
+    this.continueConnect()
+  }
+  getPromiseUrl() {
+    return this.urls[this.allConnect - 1]
+  }
+  /**
+   * 生成剩余的promise
+   */
+  continueConnect() {
+    if (this.allConnect > 0) {
+      Promise.race(this.promiseList).then(() => {
+        this.promiseList[this.successIndex] = this.createPromise(
+          this.getPromiseUrl(),
+          this.successIndex
+        )
+        this.continueConnect()
+      })
+    }
+  }
 }
-initPromise()
-after()
+let urls = Array(10)
+  .fill('urls')
+  .map((v, i) => {
+    return `${v}.${i}`
+  })
+let plist = new PromiseLists(urls)
+plist.initPromise()
