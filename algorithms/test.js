53bde9f40e1ccf607249bc9eafa5ed838863311c
lywTYH <763613923@qq.com>
2018-06-29 18:28:16 +0800

    add source code

--- algorithms/test.js
+++
@@ -1,8 +0,0 @@
-function hash(key) {
-  const hah = Array.from(key).reduce(
-    (hashAccumulator, keySymbol) => (hashAccumulator + keySymbol.charCodeAt(0)),
-    0,
-  );
-  return hah % 32;
-}
-hash('test');
