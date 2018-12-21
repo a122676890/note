4122d96ca1acfa6f36f1b5864026993f2ff7c7a2
lywTYH <763613923@qq.com>
2018-12-21 15:09:45 +0800

    edit test case

--- algorithms/benchmark/Do_not_use_splice_in_a_loop.js
+++ algorithms/benchmark/Do_not_use_splice_in_a_loop.js
@@ -8,19 +8,19 @@ function arrayInsert(target, insertIndex, insertArr) {
   return before.concat(insertArr, after);
 }
 
-
+const num = 10000;
 // 添加测试
 suite
   .add('ha splice', () => {
-    const array1 = [1, 2, 3];
-    for (let index = 0; index < 3; index++) {
+    const array1 = new Array(10000).fill(1);
+    for (let index = 0; index < 10000; index++) {
       array1.splice(index, 0, 1);
     }
   })
   .add('no splice', () => {
-    let array2 = [1, 2, 3];
+    let array2 = new Array(10000).fill(1);
     const newarr = [];
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < 10000; index++) {
       newarr.push(1);
     }
     array2 = arrayInsert(array2, 0, newarr);
